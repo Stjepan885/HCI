4ef23d110e0cc31d9ffe7f8fcff0f9915fd41cd2
Stjepan885 <turk.stipe@gmail.com>
2022-01-06 14:52:42 +0100

    acceleration session, dela vise manje

--- app/src/main/java/com/example/myapplication/AccelerometerActivityTest.java
+++ app/src/main/java/com/example/myapplication/AccelerometerActivityTest.java
@@ -11,10 +11,11 @@ public class AccelerometerActivityTest extends AppCompatActivity {
 
     private TextView xText, yText, zText;
     private Accelerometer accelerometer;
-    private int counter = 10, zCounter = 10, yCounter3 = 0;
+    private int counter = 10, zCounter = 10, yCounter3 = 0, counterTwo = 2;
     private float imageX, imageY, defaultImageX, defaultImageY, defaultX, defaultY;
     private ImageView image;
     private float accSumX = 0, accSumY = 0, accSumZ = 0;
+    private boolean set = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,8 +40,21 @@ public class AccelerometerActivityTest extends AppCompatActivity {
 
 
         accelerometer.setListener(new Accelerometer.Listener() {
+
             @Override
             public void onTranslation(float x, float y, float z) {
+
+                if (counterTwo > 0 && set == true){
+                    counterTwo--;
+                }
+                if (accSumX >= 3 || accSumX <= -3f || y >= 3 || y <= -3f || z >= 3 || z <= -3f) {
+                    if (counter == 0 && set == false) {
+                        counterTwo = 4;
+                        set = true;
+                    }
+                }
+
+                xText.setText("" + counterTwo + " " + set);
 
                 accSumX += x;
                 accSumY += y;
@@ -51,60 +65,75 @@ public class AccelerometerActivityTest extends AppCompatActivity {
                 }
 
 
-
                 //xText.setText("" + String.format("%.2f", accSumX));
                 //yText.setText("" + String.format("%.2f", accSumY));
                 yText.setText("" + String.format("%.2f", accSumX));
 
+                // left right
                 accSumX = accSumX - (accSumX * 0.2f);
                 accSumY = accSumY - (accSumY * 0.2f);
-
-                    if (x >= 2f && counter == 0) {
-                        image.setX(image.getX() - 250);
-                        counter = 15;
-                        xText.setText("right");
-                    }else if (x <= -2f && counter == 0) {
-                        image.setX(image.getX() + 250);
-                        counter = 15;
-                        xText.setText("left");
-                    }
+                accSumZ = accSumZ - (accSumZ * 0.2f);
 
 
-                    if (accSumY >= 2f && counter == 0) {
-                        image.setY(image.getY() + 250);
-                        counter = 15;
-                        xText.setText("dole");
-                    }
-                    if (accSumY <= -2f && counter == 0) {
-                        image.setY(image.getY() - 250);
-                        counter = 15;
-                        xText.setText("gore");
-                    }
+                zText.setText("" + counter);
+
+                if (accSumX >= 1.5f && counterTwo == 0) {
+                    image.setX(image.getX() + 250);
+                    counter = 10;
+
+                    set = false;
+                    counterTwo = 3;
+                } else if (accSumX <= -1.5f && counterTwo == 0) {
+                    image.setX(image.getX() - 250);
+                    counter = 10;
+                    zText.setText("left");
+                    set = false;
+                    counterTwo = 3;
+                }
+                if (counter > 0) {
+                    counter--;
+                }
+
+                if (accSumY >= 1.5f && counterTwo == 0) {
+                    image.setY(image.getY() - 250);
+                    counter = 10;
+
+                    set = false;
+                    counterTwo = 3;
+                }
+                if (accSumY <= -1.5f && counterTwo == 0) {
+                    image.setY(image.getY() + 250);
+                    counter = 10;
+
+                    set = false;
+                    counterTwo = 3;
+                }
 
 
                 //zoom
-                accSumZ = accSumZ - (accSumZ * 0.2f);
-                if (accSumZ >= 3 && counter == 0) {
-                     zoomIn();
+                if (accSumZ >= 2 && counterTwo == 0) {
+                    zoomIn();
                     xText.setText("zoom in");
-                    counter  = 10;
-                }
-                if (accSumZ <= -3 && counter == 0 && imageX == defaultImageX){
                     counter = 10;
+                    set = false;
+                    counterTwo = 3;
                 }
-                if (accSumZ <= -3 && counter == 0 && imageX != defaultImageX) {
-                     zoomOut();
+                if (accSumZ <= -2 && counterTwo == 0 && imageX == defaultImageX) {
+                    counter = 10;
+                    set = false;
+                    counterTwo = 3;
+                }
+                if (accSumZ <= -2 && counterTwo == 0 && imageX != defaultImageX) {
+                    zoomOut();
                     xText.setText("zoom out");
                     counter = 10;
+                    set = false;
+                    counterTwo = 3;
                 }
-
                 zText.setText(counter + "");
                 //-zoom
 
 
-                if (counter > 0) {
-                    counter--;
-                }
             }
 
             private void zoomOut() {
